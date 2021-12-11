ef50338a5b8bd7155cdaf13c593b16cd867331be
Dmitriy <dm.android@gmail.com>
2021-12-11 07:25:56 +0300

    Initial Commit

---
+++ MixTheColrors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MixTheColrors
+//
+//  Created by Дмитрий Шульгин on 11.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
